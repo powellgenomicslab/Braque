0bd7bbcbd365aabae6c27c0d4f1eb848fec1e0f3
joseah <alquicirajose@gmail.com>
2021-11-09 23:13:03 +1100

    adds first iteration of package

---
+++ R/generics.R
@@ -0,0 +1,12 @@
+#' @title Retrieve metadata
+#' @describeIn metadata Retrieve metadata
+setGeneric("metadata",  function(object) {
+  standardGeneric("metadata")
+})
+
+#' @title Retrieve expression data
+#' @describeIn expr Retrieve expression data
+setGeneric("expr",  function(object) {
+  standardGeneric("expr")
+})
+
